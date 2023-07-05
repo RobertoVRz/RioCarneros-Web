891e9af0d9cbefc55527e9f56c14a530a5625c74
RobertoVRz <roberto.vzrz@gmail.com>
2023-07-05 05:11:34 -0600

    feat: Diagnostico terminado

--- src/pages/diagnostico.jsx
+++ src/pages/diagnostico.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function diagnostico() {
   return (
-    <div className="text-justify">
+    <div className="py-28 px-5 lg:px-10">
       <div className="flex flex-col md:flex-row">
-        <div className="m-12 w-3/4 md:w-1/2">
-          <h1 className="mb-5 text-2xl">Diagnostico</h1>
-          <p className="mb-5">
+        <div className="w-full lg:w-1/2">
+          <h1 className="mb-10 text-5xl text-primary">Diagnostico</h1>
+          <p className="mb-5 text-2xl">
             La investigación por parte de la Maestría en Gestión Ambiental para
             la Sustentabilidad y la SEDEMA de trabajo de recopilación de datos
             en campo, imágenes satelitales y una gran cantidad de análisis de
@@ -14,290 +14,106 @@ export default function diagnostico() {
             de un diagnóstico e información indispensable para caracterizar el
             territorio.
           </p>
-          <p>
+          <p className="text-2xl mb-5">
             Se identificaron las zonas funcionales de la nanocuenca, que se
             dividen en alta donde se da la captación de agua, en la zona media
             hay un incremento en el almacenamiento de agua que pasa a la zona
             baja o de descarga.{" "}
           </p>
         </div>
-        <div className="w-1/2 m-5 md:m-10">
-          <table className="table w-full">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Zona funcional</th>
-                <th>Msnm</th>
-                <th>ha</th>
-                <th>%</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* row 1 */}
-              <tr>
-                <td>Alta (captación)</td>
-                <td>1,615 - 1,925</td>
-                <td>291</td>
-                <td>16%</td>
-              </tr>
-              {/* row 2 */}
-              <tr>
-                <td>Media(almacenamiento)</td>
-                <td>1,450 - 1,614</td>
-                <td>706</td>
-                <td>40%</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <td>Baja(descarga)</td>
-                <td>1,361 - 1,449</td>
-                <td>776</td>
-                <td>44%</td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="w-full lg:w-1/2 px-5">
+          <img
+            src="https://static.wixstatic.com/media/6a2275_17af5dfdd6ec4d1ca09ca7f432a037ef~mv2.png/v1/crop/x_16,y_0,w_796,h_669/fill/w_687,h_577,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/Zonas%20funcionales%20de%20la%20nanocuenca.png"
+            alt="Zonas funcionales de la nanocuenca"
+            className="w-full"
+          />
+          <img
+            src="https://static.wixstatic.com/media/6a2275_9312999583e64ffa99d9720a966df9c7~mv2.png/v1/crop/x_0,y_0,w_1525,h_330/fill/w_657,h_129,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/tabla%20zonas%20funcionales%20nanocuenca.png"
+            alt="Zonas funcionales de la nanocuenca"
+            className="w-full"
+          />
         </div>
       </div>
-      <div className="m-5">
-        <p className="m-5">
-          También con análisis de datos topográficos que permiten conocer el
-          relieve de la zona y tener información precisa de donde están las
-          escorrentías que alimentan al cauce principal del río Carneros y las
-          zonas propensas a derrumbes.
-        </p>
-        <p className="m-5">
-          El 68% de la cuenca con pendientes susceptibles a deslizamientos de
-          ladera (deslaves).
-        </p>
+      <div className="flex flex-col md:flex-row mt-20">
+        <div className="w-full lg:w-1/2">
+          <p className="text-2xl">
+            También con análisis de datos topográficos que permiten conocer el
+            relieve de la zona y tener información precisa de donde están las
+            escorrentías que alimentan al cauce principal del río Carneros y las
+            zonas propensas a derrumbes.
+          </p>
+          <p className="text-2xl">
+            El 68% de la cuenca con pendientes susceptibles a deslizamientos de
+            ladera (deslaves).
+          </p>
+        </div>
+        <div className="w-full lg:w-1/2 px-5">
+          <img
+            src="https://static.wixstatic.com/media/6a2275_45d8c18ef14b4d0fabeab8ce1a3e0e92~mv2.png/v1/crop/x_28,y_0,w_767,h_644/fill/w_576,h_484,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/pendientes.png"
+            alt="Zonas funcionales de la nanocuenca"
+            className="w-full"
+          />
+        </div>
       </div>
-      <div className="flex flex-col md:flex-row">
-        <div className="m-5">
-          <p className="m-5">
+      <div className="w-full">
+        <img
+          src="https://static.wixstatic.com/media/6a2275_481691102c424a13b08fa7dac9288332~mv2.png/v1/fill/w_756,h_577,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/recursos%20forestales.png"
+          alt="Zonas funcionales de la nanocuenca"
+          className="lg:w-1/2 my-10 mx-auto"
+        />
+      </div>
+      <div className="flex flex-col md:flex-row py-10">
+        <div className="w-full lg:w-1/2">
+          <p className="text-2xl mb-5">
             Al ser la parte baja donde se descarga el agua -ahora sin ser
             retenida por la vegetación por el crecimiento de la ciudad-.
           </p>
-          <p className="m-5">
+          <p className="text-2xl mb-5">
             Es donde existe la mayor cantidad de problemáticas ante las
             consecuencias de la pérdida de los servicios ecosistémicos que
             provee la vegetación.
           </p>
-          <p className="m-5">
+          <p className="text-2xl mb-5">
             12 hectáreas arboladas perdidas en 20 años. 64% (1,133 ha) de la
             nanocuenca sin arbolado.
           </p>
         </div>
-        <div className="w-1/2 m-2 md:m-10">
-          <table className="table w-full">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Zona funcional</th>
-                <th>Pérdida forestal 2001-2020 (ha)</th>
-                <th>%</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* row 1 */}
-              <tr>
-                <td>Alta o de captación</td>
-                <td>2</td>
-                <td>12%</td>
-              </tr>
-              {/* row 2 */}
-              <tr>
-                <td>Media o de almacenamiento</td>
-                <td>7</td>
-                <td>59%</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <td>Baja o de descarga</td>
-                <td>4</td>
-                <td>29</td>
-              </tr>
-            </tbody>
-          </table>
-          <div className="w-1/2 m-2 md:m-10">
-            <table className="table w-full">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th>Zona funcional</th>
-                  <th>Análisis</th>
-                  <th>Superficie(ha)</th>
-                  <th>%</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* row 1 */}
-                <tr>
-                  <td rowSpan={4}>Alta</td>
-                  <td>Arbolado denso</td>
-                  <td>98</td>
-                  <td>34%</td>
-                </tr>
-                <tr>
-                  <td>Arbolado</td>
-                  <td>109</td>
-                  <td>38%</td>
-                </tr>
-                <tr>
-                  <td>Sin arbolado</td>
-                  <td>83</td>
-                  <td>29%</td>
-                </tr>
-                <tr>
-                  <td>Subtotal</td>
-                  <td>291</td>
-                  <td>100%</td>
-                </tr>
-                {/* row 2 */}
-                <tr>
-                  <td rowSpan={4}>Medio</td>
-                  <td>Arbolado denso</td>
-                  <td>146</td>
-                  <td>21%</td>
-                </tr>
-                <tr>
-                  <td>Arbolado</td>
-                  <td>111</td>
-                  <td>16%</td>
-                </tr>
-                <tr>
-                  <td>Sin arbolado</td>
-                  <td>449</td>
-                  <td>64%</td>
-                </tr>
-                <tr>
-                  <td>Subtotal</td>
-                  <td>706</td>
-                  <td>100%</td>
-                </tr>
-                {/* row 3 */}
-                <tr>
-                  <td rowSpan={4}>Medio</td>
-                  <td>Arbolado denso</td>
-                  <td>122</td>
-                  <td>16%</td>
-                </tr>
-                <tr>
-                  <td>Arbolado</td>
-                  <td>52</td>
-                  <td>7%</td>
-                </tr>
-                <tr>
-                  <td>Sin arbolado</td>
-                  <td>601</td>
-                  <td>78%</td>
-                </tr>
-                <tr>
-                  <td>Subtotal</td>
-                  <td>776</td>
-                  <td>100%</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+        <div className="w-full lg:w-1/2 px-5">
+          <img
+            src="https://static.wixstatic.com/media/6a2275_80aa0051ed104a45a8f221e05cc0dfc5~mv2.png/v1/fill/w_600,h_133,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/tabla%20perdida%20forestal.png"
+            alt="Zonas funcionales de la nanocuenca"
+            className="w-full mb-5"
+          />
+          <img
+            src="https://static.wixstatic.com/media/6a2275_a8198bc919d44caa9e9f67b7ad738b5c~mv2.png/v1/fill/w_600,h_418,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/tabla%202%20perdida%20forestal.png"
+            alt="Zonas funcionales de la nanocuenca"
+            className="w-full"
+          />
         </div>
       </div>
-      <div className="overflow-x-auto m-20">
-        <table className="table w-full">
-          {/* head */}
-          <thead>
-            <tr>
-              <th>Zona alta</th>
-              <th>Zona media</th>
-              <th>Zona baja</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <td>Reforestación</td>
-              <td>Reforestación ribereña</td>
-              <td>Desazolve del río</td>
-            </tr>
-            {/* row 2 */}
-            <tr>
-              <td>Agroforestería</td>
-              <td>Talleres de sensibilidad ambiental</td>
-              <td>Limpieza de tragatormentas y alcantarillas</td>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <td>Talleres de sensibilidad ambiental</td>
-              <td>
-                Aplicar(generar) reglamentos de medio ambiente y/o seguridad
-              </td>
-              <td>Estabilización de taludes</td>
-            </tr>
-            <tr>
-              <td>
-                Aplicar(generar) reglamentos de medio ambiente y/o seguridad
-              </td>
-              <td>Infraestrucura(aguas residuales)</td>
-              <td>Prevención de deslizamiento</td>
-            </tr>
-            <tr>
-              <td>Establecimiento de parques lineales</td>
-              <td>Reparación de infraestructura</td>
-              <td>Reforestación urbana ribereña y en taludes</td>
-            </tr>
-            <tr>
-              <td>
-                Permanencia o generación de mesa de trabajo interinstitucional
-              </td>
-              <td>
-                Permanencia o generación de mesa de trabajo interinstitucional
-              </td>
-              <td>Humedal artificial</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>Talleres de senbilización ambiental</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>Talleres de manejo de residuos</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>Ubicación de contenedores para residuos</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>
-                Aplicar(generar) reglamentos de medio ambiente y/o seguridad
-              </td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>Infraestrucura(aguas residuales)</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>Reparación de infraestructura</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>Establecimiento de parques lineales</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>
-                Permanencia o generación de mesa de trabajo interinstitucional
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="w-full">
+        <img
+          src="https://static.wixstatic.com/media/6a2275_42ff853229414cd9b46c9791b2436c50~mv2.png/v1/crop/x_27,y_0,w_755,h_634/fill/w_687,h_577,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/la%20problematica.png"
+          alt="Zonas funcionales de la nanocuenca"
+          className="lg:w-1/2 my-10 mx-auto"
+        />
+      </div>
+      <div className="w-full flex flex-wrap items-start">
+        <img
+          src="https://static.wixstatic.com/media/6a2275_acfb2f8064124602b15cdded15ebaede~mv2.png/v1/crop/x_0,y_7,w_641,h_1033/fill/w_307,h_504,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/acciones%20especificas%20zona%20alta.png"
+          alt="Zonas funcionales de la nanocuenca"
+          className="object-contain my-10 mx-auto"
+        />
+        <img
+          src="https://static.wixstatic.com/media/6a2275_7481ad519c774115b05159b7fd7b370c~mv2.png/v1/fill/w_353,h_504,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/acciones%20especificas%20zona%20media.png"
+          alt="Zonas funcionales de la nanocuenca"
+          className="object-contain my-10 mx-auto"
+        />
+        <img
+          src="https://static.wixstatic.com/media/6a2275_063b832769ff4ea4abf574d4ae42a1ea~mv2.png/v1/fill/w_370,h_757,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/acciones%20especificas%20zona%20baja.png"
+          alt="Zonas funcionales de la nanocuenca"
+          className="object-contain my-10 mx-auto"
+        />
       </div>
     </div>
   );
